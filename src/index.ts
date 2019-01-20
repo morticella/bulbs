d9c1837dbb39be6bb3eacd01f195e24f260ff8d3
morticella <info@morticella.com>
2019-01-20 19:52:43 +0100

    I used to use ; in angular here not necessary

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import { range } from 'rxjs';
+import { range } from 'rxjs'
 
-let switchOn: boolean = false;
-let count: number = 0;
+let switchOn: boolean = false
+let count: number = 0
 
-const bulbs = range(1,10)
+const bulbs = range(1,100)
 
 const bulbsResults = bulbs.subscribe(
     bulb => {
         for (let i = 1; i <= bulb; i++) {
             if(bulb%i===0){
-                count++;
+                count++
             }   
         }
         if (count%2 === 0){
-            switchOn = false;
+            switchOn = false
         } else {
-            switchOn = true;
+            switchOn = true
         }
         count = 0;
         console.log(`Bulb n° ${bulb} is ${switchOn}`)

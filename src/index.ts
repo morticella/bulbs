b76b6823fd759c3cda1b3484461ac3f6c7027c4e
morticella <info@morticella.com>
2019-01-21 13:36:17 +0100

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { range } from 'rxjs'
 
 let switchOn = false
-let count = 0
+let countClicks = 0
 
 const bulbs = range(1,100)
 
@@ -9,11 +9,11 @@ const bulbsResults = bulbs.subscribe(
     bulb => {
         for (let i = 1; i <= bulb; i++) {
             if(bulb%i === 0){
-                count++
+                countClicks++
             }   
         }
-        switchOn = (count%2 === 0) ? switchOn = false : switchOn = true
-        count = 0
+        switchOn = (countClicks%2 === 0) ? switchOn = false : switchOn = true
+        countClicks = 0
 
         console.log(`Is the Bulb n° ${bulb} on? ${switchOn}`)
     }
